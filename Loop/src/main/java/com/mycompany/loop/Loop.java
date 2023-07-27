c86c0813d6a33ea47a7a1d5899c169b14f0117db
Yasintha8 <135942733+Yasintha8@users.noreply.github.com>
2023-07-27 23:44:53 +0530

    Practical 01-Q3-2023-/07/27

---
+++ Loop/src/main/java/com/mycompany/loop/Loop.java
@@ -0,0 +1,14 @@
+package com.mycompany.loop;
+public class Loop 
+{
+
+    public static void main(String[] args)
+    {
+      int x=0;
+      while(x<=4)
+      {
+          System.out.println("Executing loop "+x);
+          x++;
+      }
+    }
+}
