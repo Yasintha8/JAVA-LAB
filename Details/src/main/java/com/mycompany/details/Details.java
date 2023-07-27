ff2655fc3ae71d3917824ec004357285aa0e3526
Yasintha8 <135942733+Yasintha8@users.noreply.github.com>
2023-07-27 23:44:27 +0530

    Practical 01-Q2-2023-/07/27

---
+++ Details/src/main/java/com/mycompany/details/Details.java
@@ -0,0 +1,10 @@
+package com.mycompany.details;
+public class Details 
+{
+
+  public static void main(String[] args) 
+    {
+        System.out.println("My name is yasintha");
+         System.out.println("My degree program is software engineering");
+    }
+}
